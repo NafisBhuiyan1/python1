4e2e2a36796fa35d381f6fac9b78af332d05dc0b
NafisBhuiyan1 <70105519+NafisBhuiyan1@users.noreply.github.com>
2021-06-06 12:07:26 -0400

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,22 @@
+Guess a number (between 1 and 9):
+Enter your guess:- 1
+Your guess was too low: Guess a number higher than 1
+Enter your guess:- 3
+Your guess was too low: Guess a number higher than 3
+Enter your guess:- 5
+Your guess was too low: Guess a number higher than 5
+Enter your guess:- 7
+Your guess was too low: Guess a number higher than 7
+Enter your guess:- 9
+Your guess was too high: Guess a number lower than 9
+enter your guess:- 8
+YOU WON!!
+
+
+while chances 5
+
+if guess === number :
+    print ("YOU WON!!")
+
+    if not chances < 5:
+        print ("YOU LOSE!! The number is 8s", number)
